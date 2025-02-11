e051473a2984116ba7f5d3aaec03a8296f963763
haolin16 <1484239323@qq.com>
2025-02-11 14:20:53 +0800

    Create fileB.py

---
+++ fileB.py
@@ -0,0 +1,5 @@
+print('asfsdafasdfasdfsadfsdafasdf')
+print('asfsdafasdfasdfsadfsdafasdf')
+print('asfsdafasdfasdfsadfsdafasdf')
+print('asfsdafasdfasdfsadfsdafasdf')
+print('asfsdafasdfasdfsadfsdafasdf')
